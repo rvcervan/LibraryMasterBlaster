25f0f3088048c568cde271870fc106ca81375cbf
Raul Cervantes <cervantesvraul@gmail.com>
2022-09-01 13:14:46 -0700

    Test Commit

---
+++ src/main/java/com/example/LibraryMasterBlaster/LibraryMasterBlasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryMasterBlaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMasterBlasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMasterBlasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LibraryMasterBlaster/LibraryMasterBlasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryMasterBlaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMasterBlasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
